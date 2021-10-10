1949c52dc63170cf4b43fcfd50608073d08e288b
pedrosc1967 <pedrosc1967@gmail.com>
2021-10-10 14:15:02 +0200

    Google Service Json added for Android

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,10 +2,10 @@ class Constants {
   Constants._();
   static const double padding = 20;
   static const double avatarRadius = 45;
-  static const String bannerAndroid = '1330190004069862_1330190360736493';
-  static const String bannerIOS = '1330190004069862_1330224187399777';
-  static const String interstitialAndroid = '1330190004069862_1469598040129057';
-  static const String interstitialIOS = '1330190004069862_1469593800129481';
+  // static const String bannerAndroid = '1330190004069862_1330190360736493';
+  // static const String bannerIOS = '1330190004069862_1330224187399777';
+  // static const String interstitialAndroid = '1330190004069862_1469598040129057';
+  // static const String interstitialIOS = '1330190004069862_1469593800129481';
   static const String URLOtherAppsAndroid =
       'https://play.google.com/store/apps/dev?id=8134258655084518574';
   static const String URLOtherAppsIOS =
